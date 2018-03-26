e66111561ea7797198899664d6f47046827fa68c
Thoai Vo <vokythoai@gmail.com>
2018-03-26 14:50:31 +0700

    Add more options

--- app/services/telegram_bot_process_service.rb
+++ app/services/telegram_bot_process_service.rb
@@ -1,24 +1,30 @@
 class TelegramBotProcessService
   class << self
     def check_scammer message
-      BotResponse.remind.pluck(:key_word).each do |word|
-        if message["text"].gsub(/\s+/, '') =~ /#{word}/
-          bot_response = BotResponse.where(key_word: word).first
-          TelegramBotResponse.new.remind_and_send_warning_message(message, bot_response)
+      BotResponse.remind.pluck(:key_word).each do |key_word|
+        key_word.split(",").each do |word|
+          if message["text"].gsub(/\s+/, '') =~ /#{word}/
+            bot_response = BotResponse.where(key_word: word).first
+            TelegramBotResponse.new.remind_and_send_warning_message(message, bot_response)
+          end
         end
       end
 
-      BotResponse.ban.pluck(:key_word).each do |word|
-        if message["text"].gsub(/\s+/, '') =~ /#{word}/
-          bot_response = BotResponse.where(key_word: word).first
-          TelegramBotResponse.new.remove_user_from_chat(message, bot_response)
+      BotResponse.ban.pluck(:key_word).each do |key_word|
+        key_word.split(",").each do |word|
+          if message["text"].gsub(/\s+/, '') =~ /#{word}/
+            bot_response = BotResponse.where(key_word: word).first
+            TelegramBotResponse.new.remove_user_from_chat(message, bot_response)
+          end
         end
       end
 
-      BotResponse.remove_message.pluck(:key_word).each do |word|
-        if message["text"].gsub(/\s+/, '') =~ /#{word}/
-          bot_response = BotResponse.where(key_word: word).first
-          TelegramBotResponse.new.delete_message(message, bot_response)
+      BotResponse.remove_message.pluck(:key_word).each do |key_word|
+        key_word.split(",").each do |word|
+          if message["text"].gsub(/\s+/, '') =~ /#{word}/
+            bot_response = BotResponse.where(key_word: word).first
+            TelegramBotResponse.new.delete_message(message, bot_response)
+          end
         end
       end
       return false
