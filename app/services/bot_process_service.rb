2630b44bf2083eca6fab481ff2bbadd72322778e
Thoai Vo <vokythoai@gmail.com>
2018-04-03 11:49:57 +0700

    Debugger

--- app/services/bot_process_service.rb
+++ app/services/bot_process_service.rb
@@ -8,7 +8,7 @@ class BotProcessService
     BotResponse.remind.pluck(:key_word).each do |key_word|
       bot_response = BotResponse.where(key_word: key_word).first
       key_word.split(",").each do |word|
-        if message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/ || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
+        if (message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/) || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
           @service.new.remind_and_send_warning_message(message, bot_response, client)
         end
       end
@@ -17,7 +17,7 @@ class BotProcessService
     BotResponse.ban.pluck(:key_word).each do |key_word|
       bot_response = BotResponse.where(key_word: key_word).first
       key_word.split(",").each do |word|
-        if message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/ || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
+        if (message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/) || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
           @service.new.remove_user_from_chat(message, bot_response, client)
         end
       end
@@ -26,7 +26,11 @@ class BotProcessService
     BotResponse.remove_message.pluck(:key_word).each do |key_word|
       bot_response = BotResponse.where(key_word: key_word).first
       key_word.split(",").each do |word|
-        if message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/ || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
+        p "==============================="
+        p Regexp.new(word)
+        p message["text"].gsub(/\s+/, '')
+        p message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))
+        if (message["text"].gsub(/\s+/, '').downcase =~ /#{word.gsub(/\s+/, '').downcase}/) || (message["text"].gsub(/\s+/, '') =~ (Regexp.new(word))).present?
           @service.new.delete_message(message, bot_response, client)
         end
       end
